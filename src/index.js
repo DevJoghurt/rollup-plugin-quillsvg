93d28896cd990e875ae8d025a4a8c2b7144311c7
Johannes Hof <johannes@disedia.com>
2017-07-24 14:20:22 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import {
+	extname
+} from 'path';
+import {
+	createFilter
+} from 'rollup-pluginutils'
+
+export default function svg(options = {}) {
+	const filter = createFilter(options.include, options.exclude)
+
+	return {
+		name: 'quillsvg',
+
+		transform(code, id) {
+
+			if (!filter(id) || extname(id) !== '.svg') {
+				return null
+			}
+
+			const mime = 'image/svg+xml'
+			const buffer = Buffer.from(code.trim(), 'utf-8')
+			const encoded = buffer.toString('base64')
+			const exported = `export default '<img src="data:${mime};base64,${encoded}">'`
+
+			return {
+				code: exported,
+				map: {
+					mappings: ''
+				}
+			}
+		}
+	}
+}
